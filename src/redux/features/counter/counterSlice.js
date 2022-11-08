68762f05e8c66e8889da0664bc28767b95665c68
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-11-08 21:11:35 +0100

    Full Counter App Example async

--- src/redux/features/counter/counterSlice.js
+++ src/redux/features/counter/counterSlice.js
@@ -28,10 +28,12 @@ export const { increment, decrement, incrementByAmount } = counterSlice.actions
 // can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
 // will call the thunk with the `dispatch` function as the first argument. Async
 // code can then be executed and other actions can be dispatched
-export const incrementAsync = amount => dispatch => {
-  setTimeout(() => {
-    dispatch(incrementByAmount(amount))
-  }, 1000)
+const sleepAwait = sec => new Promise(resolve => {
+  setTimeout(_ => resolve(), sec * 1000)
+})
+export const incrementAsync = amount => async dispatch => {
+  await sleepAwait(1)
+  dispatch(incrementByAmount(amount))
 }
 
 // The function below is called a selector and allows us to select a value from
