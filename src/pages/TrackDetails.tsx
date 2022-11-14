ff24307cebfe16f41842ae964762931b1a264efb
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-11-15 00:58:02 +0100

    TrackDetails typed

--- src/pages/TrackDetails.tsx
+++ src/pages/TrackDetails.tsx
@@ -1,18 +1,43 @@
 import { useParams } from 'react-router-dom'
 import { useTrackDetailsQuery } from '../redux/services/musicApi'
 
+type TError = {
+  status: number,
+  data: string
+}
+
+
+type TData = {
+  // [key: string]:  { [key: string]: string },
+  images: { [key: string]: string },
+  genres: { [key: string]: string },
+  hub: { actions: { uri?: string }[] }
+}
+
+type TUseQuery = {
+  data: TData,
+  isLoading: boolean,
+  isFetching: boolean,
+  error: TError
+}
+
 const TrackDetails = () => {
   const { trackDetailsId } = useParams()
-  const { data, isLoading } = useTrackDetailsQuery(trackDetailsId)
+  const { data, isLoading, isFetching, error } = useTrackDetailsQuery<TUseQuery>(trackDetailsId)
 
-  if (isLoading) return <h1>LOADING....................</h1>
+  if (isLoading || isFetching) return <h1>LOADING....................</h1>
 
-  const { images: { background, coverart }, genres: { primary } } = data
-
+  const { images: { background, coverart }, genres: { primary }, hub: { actions } }: TData = data ?? { images: { background: '', coverart: '' }, genres: { primary: '' }, hub: { actions: '' } }
+  console.log('action', actions[1]?.uri)
+  console.log('error', error?.status)
   return (
     <>
       <h1>SongDeatils: {trackDetailsId}</h1>
       <h3>{primary}</h3>
+      <audio
+        src={actions[1]?.uri}
+        autoPlay
+      />
       <div>
         <img src={background} />
         <img src={coverart} />
